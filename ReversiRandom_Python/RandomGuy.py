31e3a52ebbb0e498bc0b9c8d9155cafaf0561bd0
tarthir <tylerbrady34@gmail.com>
2019-09-23 14:59:53 -0600

    Made the file a class. Basic recursion done. Need to add heuristic and base case

--- ReversiRandom_Python/RandomGuy.py
+++ ReversiRandom_Python/RandomGuy.py
@@ -1,172 +1,210 @@
-
 import sys
 import socket
 import time
 from random import randint
 
-t1 = 0.0    # the amount of time remaining to player 1
-t2 = 0.0    # the amount of time remaining to player 2
+t1 = 0.0  # the amount of time remaining to player 1
+t2 = 0.0  # the amount of time remaining to player 2
+class ReversiClient:
+    def __init__(self):
+        # state[0][0] is the bottom left corner of the board (on the GUI)
+        self.state = [[0 for x in range(8)] for y in range(8)]
+        self.currRound = 0
+        self.player_num = -1
+        self.best_move_so_far = None
 
-state = [[0 for x in range(8)] for y in range(8)] # state[0][0] is the bottom left corner of the board (on the GUI)
 
-# You should modify this function
-# validMoves is a list of valid locations that you could place your "stone" on this turn
-# Note that "state" is a global variable 2D list that shows the state of the game
-def move(validMoves):
-    # just return a random move
-    myMove = randint(0,len(validMoves)-1)
-    
-    return myMove
+    def determine_move(self, validMoves, tmp_state):
+        # Go through each valid move, checking for further valid moves down that "branch"
+        # At some point, assign a value to each "final"
 
-#establishes a connection with the server
-def initClient(me,thehost):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if 1 == 1:
+            # BASE CASE
+            pass
+        # TODO determine best move so far
+        for i in range(len(validMoves)):
+            # change state here
+            r = validMoves[i][0]
+            c = validMoves[i][1]
+            tmp_state[r][c] = self.player_num
+            # get new moves
+            new_moves = self.getValidMoves(self.player_num, tmp_state)
+            # Recursion
+            self.determine_move(new_moves, tmp_state)
 
-    server_address = (thehost, 3333+me)
-    print >> sys.stderr, 'starting up on %s port %s' % server_address
-    sock.connect(server_address)
-    
-    info = sock.recv(1024)
-    
-    print info
+        return best_move_so_far
+    # You should modify this function
+    # validMoves is a list of valid locations that you could place your "stone" on this turn
+    # Note that "state" is a global variable 2D list that shows the state of the game
 
-    return sock
 
-# reads messages from the server
-def readMessage(sock):
-    mensaje = sock.recv(1024).split("\n")
-    #print mensaje
+    def move(self, validMoves):
+        # just return a random move if we are in the first 4 rounds
+        if self.currRound < 4:
+            myMove = randint(0, len(validMoves) - 1)
+            return myMove
+        # else determine  the correct move
+        tmp_state = self.state.copy()
+        return self.determine_move(validMoves, tmp_state)
 
-    turn = int(mensaje[0])
-    print "Turn: " + str(turn)
 
-    if (turn == -999):
-        time.sleep(1)
-        sys.exit()
+    # establishes a connection with the server
+    def initClient(self, me, thehost):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    round = int(mensaje[1])
-    print "Round: " + str(round)
-    t1 = float(mensaje[2])  # update of the amount of time available to player 1
-    #print t1
-    t2 = float(mensaje[3])  # update of the amount of time available to player 2
-    #print t2
+        server_address = (thehost, 3333 + me)
+        print('starting up on %s port %s' % server_address, file=sys.stderr)
+        sock.connect(server_address)
 
-    count = 4
-    for i in range(8):
-        for j in range(8):
-            state[i][j] = int(mensaje[count])
-            count = count + 1
-        print state[i]
+        info = sock.recv(1024)
 
-    return turn, round
+        print(info)
 
-def checkDirection(row,col,incx,incy,me):
-    sequence = []
-    for i in range(1,8):
-        r = row+incy*i
-        c = col+incx*i
-    
-        if ((r < 0) or (r > 7) or (c < 0) or (c > 7)):
-            break
+        return sock
 
-        sequence.append(state[r][c])
 
-    count = 0
-    for i in range(len(sequence)):
-        if (me == 1):
-            if (sequence[i] == 2):
-                count = count + 1
-            else:
-                if ((sequence[i] == 1) and (count > 0)):
-                    return True
-                break
-        else:
-            if (sequence[i] == 1):
-                count = count+ 1
-            else:
-                if ((sequence[i] == 2) and (count > 0)):
-                    return True
-                break
-    
-    return False
+    # reads messages from the server
+    def readMessage(self, sock):
+        message = sock.recv(1024).decode("utf-8").split("\n")
+        # print(message)
 
-def couldBe(row, col, me):
-    for incx in range(-1,2):
-        for incy in range(-1,2):
-            if ((incx == 0) and (incy == 0)):
-                continue
+        turn = int(message[0])
+        print("Turn: " + str(turn))
 
-            if (checkDirection(row,col,incx,incy,me)):
-                return True
+        if (turn == -999):
+            time.sleep(1)
+            sys.exit()
 
-    return False
+        self.currRound = int(message[1])
+        print("Round: " + str(self.currRound))
+        # t1 = float(message[2])  # update of the amount of time available to player 1
+        # print t1
+        # t2 = float(message[3])  # update of the amount of time available to player 2
+        # print t2
 
-# generates the set of valid moves for the player; returns a list of valid moves (validMoves)
-def getValidMoves(round, me):
-    validMoves = []
-    print "Round: " + str(round)
-    
-    for i in range(8):
-        print state[i]
-
-    if (round < 4):
-        if (state[3][3] == 0):
-            validMoves.append([3, 3])
-        if (state[3][4] == 0):
-            validMoves.append([3, 4])
-        if (state[4][3] == 0):
-            validMoves.append([4, 3])
-        if (state[4][4] == 0):
-            validMoves.append([4, 4])
-    else:
+        count = 4
         for i in range(8):
             for j in range(8):
-                if (state[i][j] == 0):
-                    if (couldBe(i, j, me)):
-                        validMoves.append([i, j])
+                self.state[i][j] = int(message[count])
+                count = count + 1
+            print(self.state[i])
 
-    return validMoves
+        return turn, self.currRound
 
 
-# main function that (1) establishes a connection with the server, and then plays whenever it is this player's turn
-def playGame(me, thehost):
-    
-    # create a random number generator
-    
-    sock = initClient(me, thehost)
-    
-    while (True):
-        print "Read"
-        status = readMessage(sock)
-    
-        if (status[0] == me):
-            print "Move";
-            validMoves = getValidMoves(status[1], me)
-            print validMoves
-            
-            myMove = move(validMoves)
-        
-            sel = str(validMoves[myMove][0]) + "\n" + str(validMoves[myMove][1]) + "\n";
-            print "<" + sel + ">"
-            sock.send(sel);
-            print "sent the message"
+    def checkDirection(self, row, col, incx, incy, me):
+        sequence = []
+        for i in range(1, 8):
+            r = row + incy * i
+            c = col + incx * i
+
+            if ((r < 0) or (r > 7) or (c < 0) or (c > 7)):
+                break
+
+            sequence.append(self.state[r][c])
+
+        count = 0
+        for i in range(len(sequence)):
+            if (me == 1):
+                if (sequence[i] == 2):
+                    count = count + 1
+                else:
+                    if ((sequence[i] == 1) and (count > 0)):
+                        return True
+                    break
+            else:
+                if (sequence[i] == 1):
+                    count = count + 1
+                else:
+                    if ((sequence[i] == 2) and (count > 0)):
+                        return True
+                    break
+
+        return False
+
+
+    def couldBe(self, row, col, me):
+        for incx in range(-1, 2):
+            for incy in range(-1, 2):
+                if ((incx == 0) and (incy == 0)):
+                    continue
+
+                if (self.checkDirection(row, col, incx, incy, me)):
+                    return True
+
+        return False
+
+    # return the beginning moves
+
+
+    def check_beginning_moves(self, validMoves):
+        if (self.state[3][3] == 0):
+            validMoves.append([3, 3])
+        if (self.state[3][4] == 0):
+            validMoves.append([3, 4])
+        if (self.state[4][3] == 0):
+            validMoves.append([4, 3])
+        if (self.state[4][4] == 0):
+            validMoves.append([4, 4])
+
+    # generates the set of valid moves for the player; returns a list of valid moves (validMoves)
+
+
+    def getValidMoves(self, me, myState=None):
+        if myState == None:
+            myState = self.state
+        validMoves = []
+        print("Round: " + str(self.currRound))
+
+        for i in range(8):
+            print(myState[i])
+
+        if (self.currRound < 4):
+            self.check_beginning_moves(validMoves)
         else:
-            print "It isn't my turn"
+            for i in range(8):
+                for j in range(8):
+                    if (myState[i][j] == 0):
+                        if (self.couldBe(i, j, self.player_num)):
+                            validMoves.append([i, j])
+
+        return validMoves
 
 
-    return
+    # main function that (1) establishes a connection with the server, and then plays whenever it is this player's turn
+    # noinspection PyTypeChecker
+    def playGame(self, thehost):
+        # create a random number generator
 
+        sock = self.initClient(self.player_num, thehost)
+
+        while (True):
+            print("Read")
+            status = self.readMessage(sock)
+            self.currRound = status[1]
+            if (status[0] == self.player_num):
+                print("Move")
+                validMoves = self.getValidMoves(self.player_num)
+                print(validMoves)
+
+                myMove = self.move(validMoves)
+
+                sel = str(validMoves[myMove][0]) + "\n" + \
+                    str(validMoves[myMove][1]) + "\n"
+                print("<" + sel + ">")
+                sock.send(sel.encode("utf-8"))
+                print("sent the message")
+            else:
+                print("It isn't my turn")
 
 
 # call: python RandomGuy.py [ipaddress] [player_number]
-#   ipaddress is the ipaddress on the computer the server was launched on.  Enter "localhost" if it is on the same computer
-#   player_number is 1 (for the black player) and 2 (for the white player)
+# ipaddress is the ipaddress on the computer the server was launched on.  Enter "localhost" if it is on the same computer
+# player_number is 1 (for the black player) and 2 (for the white player)
 if __name__ == "__main__":
-
-    print 'Number of arguments:', len(sys.argv), 'arguments.'
-    print 'Argument List:', str(sys.argv)
-
-    print str(sys.argv[1])
-    
-    playGame(int(sys.argv[2]), sys.argv[1])
-
+    print('Number of arguments:', len(sys.argv), 'arguments.')
+    print('Argument List:', str(sys.argv))
+    client = ReversiClient()
+    print(str(sys.argv[1]))
+    client.player_num = int(sys.argv[2])
+    client.playGame(sys.argv[1])
